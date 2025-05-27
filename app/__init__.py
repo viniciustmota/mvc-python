1daa0c6f12d3a152b165cd1c6944ca8537ee0489
viniciustmota <motaviny140@gmail.com>
2025-05-27 17:00:41 -0300

    Subindo arquivos

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+path = os.getcwd()
+template_dir=os.path.abspath(os.path.join(path, os.pardir))
+
+template_dir = os.path.join(template_dir, 'templates')
+app = Flask(__name__, template_folder=template_dir)
+app.config.from_object('config')
+
+db = SQLAlchemy(app)
+from .models import book_models, author_model
+from .services import book_service, author_service
+from .controlers import book_controler, author_controler
